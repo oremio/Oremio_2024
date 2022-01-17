b1494159cd25b006744240535174afeacb9ee30d
oremio <578168741@qq.com>
2022-01-17 16:04:25 +0800

    2022-01-17-01&finish

---
+++ L141/main.cpp
@@ -0,0 +1,32 @@
+//url:https://leetcode-cn.com/problems/linked-list-cycle/
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *achilles, *tortoise;
+        if (head)
+            achilles = head;
+        else
+            return false;
+        tortoise = head->next;
+        while (tortoise && tortoise->next && achilles != tortoise)
+        {
+            tortoise = tortoise->next->next;
+            achilles = achilles->next;
+        }
+        if (achilles == tortoise)
+            return true;
+        else
+            return false;
+    }
+};
---
+++ L142/main.cpp
@@ -0,0 +1,38 @@
+//url:https://leetcode-cn.com/problems/linked-list-cycle-ii/
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *achilles, *tortoise;
+        if (head && head->next)
+            achilles = head;
+        else
+            return NULL;
+        //tortoise = head->next;
+        tortoise = head; // 乌龟和阿喀琉斯出发于同一起跑线。
+        while (tortoise && tortoise->next)
+        {
+            tortoise = tortoise->next->next;
+            achilles = achilles->next;
+            if(achilles == tortoise)
+                break;
+        }
+        if (achilles == tortoise){
+            achilles = head;
+            while(achilles != tortoise){
+                achilles = achilles->next;
+                tortoise = tortoise->next;
+            }
+            return achilles;
+        }        
+        else
+            return NULL;
+    }
+};
