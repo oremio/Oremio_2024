90ffd9520b1d42f305218035c1846f0145285367
oremio <578168741@qq.com>
2022-01-20 10:42:16 +0800

    2022-01-17-01&finish

---
+++ L160/main.cpp
@@ -0,0 +1,16 @@
+//url:https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+//思路：计算两条链表的长度，然后先让较长的那个链表的指针先走一段它们的差值的距离，然后再齐头并进。
+    }
+};
---
+++ L19/main.cpp
@@ -0,0 +1,18 @@
+//url:https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        //思路：slow和fast距离为n即可。
+    }
+};
