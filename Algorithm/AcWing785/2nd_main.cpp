53cf47a47164bd4c5fb45a417aee2f872d4d7aed
oremio <578168741@qq.com>
2023-07-21 16:53:19 +0800

    2023-07-21-01

---
+++ Algorithm/AcWing785/2nd_main.cpp
@@ -0,0 +1,56 @@
+// url: https://www.acwing.com/problem/content/787/
+// tutorial: https://www.acwing.com/video/227/
+
+/*
+1.确定分界点: q[1], q[(l+r)/2], q[r], random
+2.调整区间(难点): 小于等于x放左边，大于等于x放右边
+3.递归处理左右两端
+*/
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+const int N = 1e6 + 10;
+
+int n;
+int q[N];
+
+void quick_sort(int q[], int l, int r)
+{
+    if (l >= r)
+        return;
+    int x = q[l], i = l - 1, j = r + 1;
+    while (i < j)
+    {
+        while (q[++i] < x);
+        while (q[--j] > x);
+        if (i < j)
+            swap(q[i], q[j]);
+    } // 跳出while后，i在最后一个小于x的数的右边，j在第一个大于x的数的左边
+    quick_sort(q, l, j); // 这里如果右边选i，那就是i - 1
+    quick_sort(q, j + 1, r);  // ↑那么这里就是i，但如果按照上面的x = q[l]，就会产生边界问题。j与x = q[r]同理。
+}
+
+int main()
+{
+    ifstream file("input.txt");
+    if (file.is_open())
+    {
+        // cout << "Yes" << endl;
+        cin.rdbuf(file.rdbuf());
+    }
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> q[i];
+
+    quick_sort(q, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        cout << q[i] << ' ';
+    cout << endl;
+
+    return 0;
+}
---
+++ Algorithm/AcWing786/2nd_main.cpp
@@ -0,0 +1,53 @@
+//url: https://www.acwing.com/problem/content/788/
+//tutorial: https://www.acwing.com/video/228/
+
+/*
+1. 找到分界点x, q[L], q[(L + r) / 2], q[R]
+2. 左边所有数 <= x, 右边所有数 >= x
+3_1. k <= 左边的数的个数: 递归左边
+3_2. k >= 左边的数的个数: 递归右边
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int N = 100010;
+
+int n, k;
+int q[N];
+
+int quick_sort(int l, int r, int k){
+    if(l == r)
+        return q[l];
+    int x = q[l], i = l - 1, j = r + 1;
+    while(i < j){
+        while(q[ ++ i] < x)
+            ;
+        while(q[ -- j] > x)
+            ;
+        if(i < j)
+            swap(q[i], q[j]);
+    }
+    int SL = j - l + 1;
+    if (k <= SL)
+        return quick_sort(l, j, k);
+    return quick_sort(j + 1, r, k - SL);
+}
+
+int main(){
+    ifstream file("input.txt");
+    if(file.is_open()){
+        cin.rdbuf(file.rdbuf());
+    }
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i ++ )
+        cin >> q[i];
+
+    cout << quick_sort(0, n - 1, k);
+
+    return 0;
+}
